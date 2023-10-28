1eceda6d6b524f3f0bd298846133200633b830be
Erik <mariglianoerik@gmail.com>
2023-10-28 18:59:17 +0200

    Primo esercizio array modificato

--- src/EserciziArrays.java
+++ src/EserciziArrays.java
@@ -8,13 +8,52 @@ class Ex24 {
     arrayCreationAndSum();
     }
     public static void arrayCreationAndSum() {
-        int[] Numbers = {1,2,3,4,5,6,7,8,9,10};
-            for(int i = 0; i <= Numbers.length; i++){
-                for(int SumOfNumbers: Numbers) {
-                    i += SumOfNumbers;
-                }
-                System.out.println("La somma di tutti i numeri contenuti nell'array è: " + i);
+        int Numbers[] = new int[10];
+        int somma = 0;
+            for(int i = 0; i < Numbers.length; i++) {
+                Numbers[i] = i + 1;
             }
+            for(int SumOfNumbers: Numbers) {
+                     somma += SumOfNumbers;
+            }
+            System.out.println("La somma di tutti i numeri contenuti nell'array è: " + somma);
 
     }
 }
+
+/*  Scrivi un programma che contenga un metodo
+ che crea un array e lo riempie con dei caratteri.
+  Questo metodo dovrà poi stampare tutte le occorrenze
+   del carattere 'a' o 0 in caso non ce ne siano.
+ */
+ class Ex25 {
+     public static void main(String args[]) {
+        char[] Caratteri = createArray();
+        SumOfAllA(Caratteri);
+
+     }
+     public static char[] createArray() {
+         char[] Caratteri = {'a', 'b', 'c', 'd', 'a', 'a'};
+         for (int i = 0; i < Caratteri.length; i++) {
+
+         }
+         return Caratteri;
+
+     }
+     public static void SumOfAllA(char[] Caratteri) {
+         int count = 0;
+         for(char CarattereA : Caratteri) {
+             if(CarattereA == 'a') {
+                 count++;
+             }
+         }
+         if(count > 0) {
+             System.out.println("Il carattere a viene stampato " + count + "volte");
+         } else {
+             System.out.println("Il carattere a non viene stampato nessuna volta");
+         }
+     }
+
+
+}
+
