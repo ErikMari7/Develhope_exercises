4b3a91680331960fa92d8c791b868090bd0b57e5
Erik <mariglianoerik@gmail.com>
2023-10-29 16:33:02 +0100

    secondo esercizio array multidimensionali finito

--- src/EserciziArraysMultidimensionali.java
+++ src/EserciziArraysMultidimensionali.java
@@ -22,3 +22,49 @@ class Ex27 {
         System.out.println("La somma degli elementi  nella prima riga è: " + sumOf);
     }
 }
+
+/*  Scrivere un programma che contenga un metodo
+ che permette di inizializzare una matrice e di scambiare
+  le sue righe e le sue colonne stampandola a video.
+   Matrice di partenza
+1 2 3
+4 5 6
+Matrice risultato
+ 1 4
+ 2 5
+ 3 6
+
+
+ */
+
+class Ex28 {
+    public static void main(String args[]) {
+        int[][] matriceDiPartenza = {{1, 2, 3}, {4, 5, 6}};
+        int[][] matriceRisultato = ScambiaRigheConColonne(matriceDiPartenza);
+        stampaMatrice(matriceDiPartenza, "Matrice di Partenza:");
+        stampaMatrice(matriceRisultato, "Matrice Risultato:");
+    }
+
+    public static int[][] ScambiaRigheConColonne(int[][]matrice) {
+        int righe = matrice.length;
+        int colonne = matrice[0].length;
+        int[][] risultato = new int[colonne][righe];
+
+        for (int i = 0; i < righe; i++) {
+            for (int j = 0; j < colonne; j++) {
+                risultato[j][i] = matrice[i][j];
+            }
+        }
+        return risultato;
+    }
+    public static void stampaMatrice(int[][]matrice, String messaggio) {
+        System.out.println(messaggio);
+        for(int i = 0; i < matrice.length; i++) {
+            for (int j = 0 ; j < matrice[i].length; j++) {
+                System.out.print(matrice[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
