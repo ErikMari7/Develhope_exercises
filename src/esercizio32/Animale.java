79134fca94aeeb4c70591793903d7db90e6fb1f8
Erik <mariglianoerik@gmail.com>
2023-11-07 16:53:46 +0100

    Finito esercizio 32

---
+++ src/esercizio32/Animale.java
@@ -0,0 +1,7 @@
+package esercizio32;
+
+public class Animale  {
+    public void faiilVerso() {
+       System.out.println("Verso dell'animale");
+    }
+}
---
+++ src/esercizio32/Gatto.java
@@ -0,0 +1,8 @@
+package esercizio32;
+
+public class Gatto extends Animale{
+    @Override
+    public void faiilVerso() {
+        System.out.println(" Verso del gatto: Miao");
+    }
+}
