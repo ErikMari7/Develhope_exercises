1fe0c524a34bbb2014d0ceed9e9fef80d2ff7fe9
Erik <mariglianoerik@gmail.com>
2023-11-08 13:46:26 +0100

    modificato esercizio 33

--- src/esercizio33/Forma.java
+++ src/esercizio33/Forma.java
@@ -1,12 +1,20 @@
 package esercizio33;
 
 public class Forma {
-    double Val1;
+    private int base =62;
+    private int altezza = 40;
 
-    double Val2;
+    public int getBase(){
+        return base;
+    }
 
-    double area = Val1 * Val2;
-    public void calcolaArea() {
-        System.out.println("Forma: " + area);
+    public int getAltezza(){
+        return altezza;
+    }
+
+    public void calcolaArea(){
+
+        System.out.println("Per il calcolo dell'area delle forme i valori sono: " + getBase() + " e " + getAltezza());
+
     }
 }
--- src/esercizio33/Rettangolo.java
+++ src/esercizio33/Rettangolo.java
@@ -1,11 +1,9 @@
 package esercizio33;
 
 public class Rettangolo extends Forma{
-    double Val1 = 12.5;
-    double  Val2 = 14;
-    double area = Val1 * Val2;
+
     @Override
     public void calcolaArea() {
-        System.out.println("Rettangolo: " + area);
+        System.out.println("L'area del rettangolo è: " + getBase() * getAltezza());
     }
 }
