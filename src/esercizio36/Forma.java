5c62da4f0fa2fb2b8aad1af24a7189f4e3ad63b8
Erik <mariglianoerik@gmail.com>
2023-11-09 19:33:43 +0100

    finito ex36

---
+++ src/esercizio36/Forma.java
@@ -0,0 +1,5 @@
+package esercizio36;
+
+interface Forma {
+    double calcolaArea();
+}
---
+++ src/esercizio36/Main.java
@@ -0,0 +1,15 @@
+package esercizio36;
+
+//Scrivere un programma che contenga una interfaccia
+//        Forma ed un metodo chiamato calcolaArea().
+//        Crea poi due sottoclassi Rettangolo e Triangolo
+//        che implementano Forma ed implementano il metodo per il calcolo dell'area.
+
+public class Main {
+    public static void main(String[] args) {
+        Rettangolo rettangolo = new Rettangolo(7,8);
+        Triangolo triangolo = new Triangolo(10,20);
+        System.out.println("Area del rettangolo: " + rettangolo.calcolaArea());
+        System.out.println("Area del triangolo: " + triangolo.calcolaArea());
+    }
+}
