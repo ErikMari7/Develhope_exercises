16119d1ea2a5b18d2446f3beaf9f408ff9b51bb7
Erik <mariglianoerik@gmail.com>
2023-11-10 17:38:51 +0100

    finito ex37

--- src/esercizio37/Rettangolo.java
+++ src/esercizio37/Rettangolo.java
@@ -4,13 +4,10 @@ public class Rettangolo extends Forma {
     private double base;
     private double altezza;
 
-    private TipoForma tipo;
 
-
-    public Rettangolo(double base,double altezza,TipoForma tipo) {
+    public Rettangolo(double base, double altezza, TipoForma tipo) {
         this.base = base;
         this.altezza = altezza;
-        this.tipo = tipo;
     }
 
     @Override
--- src/esercizio37/Triangolo.java
+++ src/esercizio37/Triangolo.java
@@ -5,12 +5,9 @@ public class Triangolo extends Forma {
     private double base;
     private double altezza;
 
-    private TipoForma tipo;
-
     public Triangolo(double base,double altezza,TipoForma tipo) {
         this.base = base;
         this.altezza = altezza;
-        this.tipo = tipo;
     }
 
     @Override
