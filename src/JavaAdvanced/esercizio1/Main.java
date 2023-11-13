7c746c22e80e0b174cd78f299e3e3ac1f47a707e
Erik <mariglianoerik@gmail.com>
2023-11-13 16:40:36 +0100

    finito ex1 JavaAdvanced

--- src/JavaAdvanced/esercizio1/Main.java
+++ src/JavaAdvanced/esercizio1/Main.java
@@ -7,17 +7,22 @@ package JavaAdvanced.esercizio1;
 public class Main {
     public static void main(String[]args) {
         int Numero = 10;
+        System.out.println(RangeNumero(Numero));
+    }
 
-        if(Numero > 0 && Numero < 10) {
-            System.out.println(true);
-        } else {
+    public static boolean RangeNumero(int Numero) {
+
+        boolean result = true;
+
             try {
-                if(Numero <= 0 || Numero >= 10) {
+                if(Numero <= 0 || Numero > 10) {
                     throw new ArithmeticException("Numero");
                 }
             } catch (ArithmeticException e) {
                 System.out.println("C'è stato un errore: " + e);
+                result = false;
             }
-        }
+
+        return result;
     }
 }
