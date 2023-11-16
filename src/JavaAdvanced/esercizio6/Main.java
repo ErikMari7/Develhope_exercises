2efb1968e4d191b0d416f068e13eeeb3f0fb87f0
Erik <mariglianoerik@gmail.com>
2023-11-16 13:27:37 +0100

    finito ex6 JavaAdvanced

--- src/JavaAdvanced/esercizio6/Main.java
+++ src/JavaAdvanced/esercizio6/Main.java
@@ -11,32 +11,36 @@ package JavaAdvanced.esercizio6;
 public class Main {
     public static void main(String[]args) {
         //faccio l'autoboxing dei valori primitivi
-        int intValue = 5;
-        Integer integerValue = intValue;
-        double doubleValue = 12.4;
-        Double DoubleValue = doubleValue;
-        char charValue = 'a';
-        Character characterValue = charValue;
+        int primoInt = 5;
+        Integer primoInteger = primoInt;
+        int secondoInt = 10;
+        Integer secondoInteger = secondoInt;
+        double primodouble = 12.4;
+        Double primoDouble =  primodouble;
+        char primochar = 'a';
+        Character primocharacter = primochar;
 
         //faccio l'unboxing degli oggetti
-        Integer integerValue2 = 7;
-        int intValue2 =integerValue2;
-        Double DoubleValue2 = 20.2;
-        double doubleValue2 = DoubleValue2;
-        Character characterValue2 = 'b';
-        char  charValue2 = characterValue2;
+        Integer terzoInteger = 7;
+        int terzoint = terzoInteger;
+        Integer quartoInteger = 12;
+        int quartoint = quartoInteger;
+        Double secondoDouble = 20.2;
+        double secondodouble = secondoDouble;
+        Character secondoCharacter = 'b';
+        char  secondocharacter = secondoCharacter;
 
         //uso le funzioni scritte mettendo i valori creati prima
-        sommaInt(intValue,intValue2);
-        charStamp(charValue2);
-        sommaInteger(integerValue,integerValue2);
-        characterStamp(characterValue);
+        sommaInt(primoInteger,secondoInteger);
+        charStamp(primocharacter);
+        sommaInteger(terzoint,quartoint);
+        characterStamp(secondocharacter);
 
 
     }
 
     public static void sommaInt(int Int1, int Int2) {
-        int SumOf = Int1 + Int2;
+        Integer SumOf = Int1 + Int2;
         System.out.println(SumOf);
     }
 
@@ -45,7 +49,7 @@ public class Main {
     }
 
     public static void sommaInteger(Integer Integer1, Integer Integer2) {
-        Integer SumOf = Integer1 + Integer2;
+        int SumOf = Integer1 + Integer2;
         System.out.println(SumOf);
     }
 
